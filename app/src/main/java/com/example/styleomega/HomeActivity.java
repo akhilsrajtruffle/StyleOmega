0526d2fa392666712bbcac2b6603367d3fb98065
Indila Dineth <indiladineth@MacBook-Pro.local>
2019-12-23 11:37:22 +0530

    view products

--- app/src/main/java/com/example/styleomega/HomeActivity.java
+++ app/src/main/java/com/example/styleomega/HomeActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.styleomega.Model.Prevalent.Prevalent;
+import com.example.styleomega.Model.Products;
+import com.example.styleomega.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -18,14 +23,19 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -35,11 +45,17 @@ public class HomeActivity extends AppCompatActivity implements
         NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private DatabaseReference productRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        productRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
         Paper.init(this);
 
@@ -77,10 +93,10 @@ public class HomeActivity extends AppCompatActivity implements
 
         userNameTextView.setText(Prevalent.currentOnlineUser.getName());
 
-//        recyclerView = findViewById(R.id.recycler_menu);
-//        recyclerView.setHasFixedSize(true);
-//        layoutManager = new LinearLayoutManager(this );
-//        recyclerView.setLayoutManager(layoutManager);
+        recyclerView = findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this );
+        recyclerView.setLayoutManager(layoutManager);
 
     }
 
@@ -89,8 +105,40 @@ public class HomeActivity extends AppCompatActivity implements
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Products> option = new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(productRef,Products.class)
+                .build();
 
 
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(option) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder productViewHolder, int i, @NonNull Products products) {
+
+                productViewHolder.txtProductName.setText(products.getProductName());
+                productViewHolder.txtProductDescription.setText(products.getDescription());
+                productViewHolder.txtProductPrice.setText(products.getPrice()+"$");
+                Picasso.get().load(products.getImage()).into(productViewHolder.imageView);
+
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout,parent,false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
