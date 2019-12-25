630733248165ab489109055297c35f495b88aa47
indiladineth@yahoo.com <chamina1>
2019-12-26 00:35:44 +0530

    place order

--- app/src/main/java/com/example/styleomega/CartActivity.java
+++ app/src/main/java/com/example/styleomega/CartActivity.java
@@ -26,8 +26,11 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.rey.material.drawable.RevealDrawable;
 
 public class CartActivity extends AppCompatActivity {
@@ -35,8 +38,7 @@ public class CartActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private Button checkoutButton;
-    private TextView txtTotalAmount;
-
+    private TextView txtTotalAmount,txtMsg1;
     private int overallTotalPrice = 0;
 
 
@@ -54,6 +56,7 @@ public class CartActivity extends AppCompatActivity {
 
         checkoutButton = (Button) findViewById(R.id.checkout_button);
         txtTotalAmount = (TextView) findViewById(R.id.total_price);
+        txtMsg1 =(TextView) findViewById(R.id.msg1);
 
 
         checkoutButton.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +79,8 @@ public class CartActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        checkOrderStatus();
 
         final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
 
@@ -166,5 +171,48 @@ public class CartActivity extends AppCompatActivity {
         adapter.startListening();
     }
 
+    private void checkOrderStatus(){
+        DatabaseReference orderRef = FirebaseDatabase.getInstance().getReference().child("Orders").child(Prevalent.currentOnlineUser.getPhone());
+
+        orderRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+
+                    String shipmentStatus = dataSnapshot.child("status").getValue().toString();
+                    String userNmae = dataSnapshot.child("name").getValue().toString();
+
+                    if(shipmentStatus.equals("shipped")){
+
+                        txtTotalAmount.setText("Your order has been shipped");
+                        recyclerView.setVisibility(View.GONE);
+
+                        txtMsg1.setText("Your order has been shipped");
+
+                        txtMsg1.setVisibility(View.VISIBLE);
+                        checkoutButton.setVisibility(View.GONE);
+
+
+
+                    }
+                    else if(shipmentStatus.equals("not shipped")){
+
+                        txtTotalAmount.setText("Your order is not shipped yet");
+                        recyclerView.setVisibility(View.GONE);
+
+                        txtMsg1.setVisibility(View.VISIBLE);
+                        checkoutButton.setVisibility(View.GONE);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 
 }
